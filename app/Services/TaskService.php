f9ac1a1eed9ec478f8ccf8d5bfd64dbd777c7118
Anton Iosifov <iosifov.a.14@gmail.com>
2026-07-25 17:25:47 +0300

    Implement Service layer (TaskService) and CRUD operations

--- app/Services/TaskService.php
+++ app/Services/TaskService.php
@@ -2,7 +2,40 @@
 
 namespace App\Services;
 
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Collection;
+
 class TaskService
 {
+    public function getAllTasks(): Collection
+    {
+        return Task::all();
+    }
 
+    public function createTask(Task $task): Task
+    {
+        $task->title = trim($task->title);
+
+        $task->save();
+
+        return $task;
+    }
+
+    public function toggleTask(int $id): Task
+    {
+        $task = Task::findOrFail($id);
+
+        $task->is_completed = !$task->is_completed;
+
+        $task->save();
+
+        return $task;
+    }
+
+    public function deleteTask(int $id): void
+    {
+        $task = Task::findOrFail($id);
+
+        $task->delete();
+    }
 }
